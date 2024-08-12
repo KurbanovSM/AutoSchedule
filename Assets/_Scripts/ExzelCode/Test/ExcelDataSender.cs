5279deee9d019a2f07576a0e66826ccf1b2945b6
KurbanovSM <newgvardgame@gmail.com>
2024-08-12 18:08:11 +0300

    ...

---
+++ Assets/_Scripts/ExzelCode/Test/ExcelDataSender.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using TMPro;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+// Данные для записи
+[Serializable]
+public class Person
+{
+    public string name;
+    public int age;
+    public int money;
+}
+
+public class ExcelDataSender : MonoBehaviour
+{
+    [SerializeField] private List<Person> _persons;
+    [SerializeField] private TMP_Text _tmpText;
+    [SerializeField] private TMP_Text _tmpText2;
+
+    private void Start()
+    {
+        Debug.Log(Path.Combine(Application.streamingAssetsPath, "Example.xlsx").Replace("\\", "/"));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SendDataToExcel();
+        }
+    }
+
+    public void SendDataToExcel()
+    {
+        // Путь к файлу Excel
+        string filePath = Path.Combine(Application.streamingAssetsPath, "Example.xlsx").Replace("\\", "/");;
+        
+        string consoleProgramExzelHandler = Path.Combine(Application.streamingAssetsPath, "ExzelHandler/ExzelHandler.exe");
+
+        string argument = JsonConvert.SerializeObject(_persons);
+        
+        argument = argument.Replace("\"", "\\\"");
+        
+        // Запускаем .NET приложение
+        Process process = new Process();
+        process.StartInfo.FileName = consoleProgramExzelHandler; 
+        
+        // Замените "path/to/your/ExcelWriter.exe" на фактический путь к вашему .NET приложению
+
+        // Передаем параметры
+        process.StartInfo.Arguments = $"\"{filePath}\" " + $"\"{argument}\" ";
+        //process.StartInfo.UseShellExecute = false;
+        //process.StartInfo.CreateNoWindow = true;
+        // Запускаем процесс
+        process.Start();
+
+        // Ожидаем завершения процесса
+        process.WaitForExit();
+
+        Debug.Log("Данные отправлены в Excel!");
+    }
+}
